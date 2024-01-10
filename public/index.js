fdd0810b66fd486184d2abe6633e52b000246743
Rogelio <roger.cruz808@gmail.com>
2024-01-09 16:34:51 -0800

    ready to create database

---
+++ public/index.js
@@ -0,0 +1,24 @@
+$('.bookJohnny').on('click', () => {
+    const paragraph = $('.paragraph-j');
+    const originalContent = paragraph.html();
+
+    paragraph.empty();
+    paragraph.html('<h4>Call now: (619)-123-4567</h4>');
+    setTimeout(() => {
+        paragraph.empty();
+        paragraph.html(originalContent);
+    }, 5000); 
+});
+
+$('.bookCisco').on('click', () => {
+    const paragraph = $('.paragraph-c');
+    const originalContent = paragraph.html();
+
+    paragraph.empty();
+    paragraph.html('<h4>Call now: (858)-123-4567</h4>');
+
+    setTimeout(() => {
+        paragraph.empty();
+        paragraph.html(originalContent);
+    }, 5000); 
+});
